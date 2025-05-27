8bc023f552261c7150add78a5c6899344cc41980
OshanBathiya <bathiya.oshan@gmail.com>
2025-05-27 22:17:39 +0530

    wip: add 16x16 grid using js and css

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.querySelector("#container");
 
-const square = document.createElement("div");
-square.setAttribute("id", "square");
-container.appendChild(square);
+for(let i=0; i<16*16; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("id", "square");
+    container.appendChild(square);
+    console.log("Square created", i);
+}
