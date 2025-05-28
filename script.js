a5366b604a0be764abf7fdd6ca0175a3694d62f2
OshanBathiya <bathiya.oshan@gmail.com>
2025-05-29 00:31:05 +0530

    feat: implement change grid size feature

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const container = document.querySelector("#container");
+const gridSizeChangeBtn = document.querySelector("#gridSizeChangeBtn");
 
-for(let i=0; i<16*16; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("id", "square");
-    container.appendChild(square);
-}
+createGrid(16);
 
-const squares = container.querySelectorAll("div");
+gridSizeChangeBtn.addEventListener("click", () => {
+    const size = prompt("Enter the number of squares (<100) for a side:");
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", (event) => {
-        event.currentTarget.setAttribute("class", "hovered");
-    });
+    if (size === null || isNaN(size) || size > 100) {
+        alert("Invalid Input");
+    } else {
+        container.replaceChildren();
+        console.log(size);
+        createGrid(size);
+    }
 });
+
+function createGrid (size) {
+
+    for(let i=0; i<size*size; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("id", "square");
+        square.style.flexBasis = 100/size + "%";
+        container.appendChild(square);
+    }
+    
+    const squares = container.querySelectorAll("div");
+    
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", (event) => {
+            event.currentTarget.setAttribute("class", "hovered");
+        });
+    });
+}
